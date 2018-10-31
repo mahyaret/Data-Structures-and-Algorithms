6f34d3bae5fe24bc650fe4d4a0d8902a9e413874
Mahyar <mahyar.etedal@icloud.com>
2018-10-30 23:41:13 -0400

    Your branch is up-to-date with 'origin/master'.

--- Graphs/breadth-first-search.cpp
+++ Graphs/breadth-first-search.cpp
@@ -1,6 +1,7 @@
 //The bread-first-search algorithm starts at a vertex and visits, first the neighbours of it, then the neighbours of the neighbours of ti, and so on.
 //A particularly useful application of the breadth-first-search algorithm is, therefore, in computing shortest paths.
 //This algorithm is best suited to graphs represented using an adjacency list representation.
+
 #include <iostream>
 #include <vector>
 #include <queue>
