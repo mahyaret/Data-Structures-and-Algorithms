4a7cf84674e88a06b098e12668696e219b39b5d5
Mahyar <mahyar.etedal@icloud.com>
2018-11-02 08:18:10 -0400

    modified:   binary-search-tree.cpp

--- Trees/binary-search-tree.cpp
+++ Trees/binary-search-tree.cpp
@@ -22,12 +22,15 @@ public:
     if(subRoot == NULL){
       return new Node(x);
     }
-    if(x < node->data){
+    if(x < subRoot->data){
       subRoot->left = insert(subRoot->left, x);
     }else{
       subRoot->right = insert(subRoot->right,x);
     }
     return subRoot;
+  }
+  void insert(int x){
+    insert(root,x);
   }
   Node* deleteNode(Node *subRoot, int x){
     if(subRoot==NULL){
@@ -47,7 +50,7 @@ public:
     }else if(subRoot->right==NULL){
       Node* temp=subRoot->left;
       delete subRoot;
-      retrun temp;
+      return temp;
     }else{
       Node * successorParent=subRoot->right;//For sure it's on the right!
 
@@ -56,7 +59,7 @@ public:
 	successorParent=successor;
 	successor=successor->left;
       }
-      successorParrent->left=successor->right;
+      successorParent->left=successor->right;
 
       subRoot->data = successor->data;
 
@@ -67,9 +70,34 @@ public:
   void deleteNode(int x){
     deleteNode(root,x);
   }
+  void inorderTraverse(Node* subRoot){
+    if(subRoot!=NULL){
+      inorderTraverse(subRoot->left);
+      cout<<subRoot->data<<" ";
+      inorderTraverse(subRoot->right);
+    }
+  }
+  void inorderTraverse(){
+    inorderTraverse(root);
+  }
 };
 
 int main(){
-  
+  /* I want to create the following BST 
+              7 
+           /     \ 
+          3       11 
+         /  \    /  \ 
+        1    5  9    13 */
+  Tree *root = new Tree(7);
+  root->insert(3);
+  root->insert(11);
+  root->insert(1);
+  root->insert(5);
+  root->insert(9);
+  root->insert(13);
+
+  root->inorderTraverse();
+  cout<<endl;
   return 0;
 }
