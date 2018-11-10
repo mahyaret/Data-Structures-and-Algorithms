d4ea2607b490cca8a81b964d2ebf996afb231d7d
Mahyar <mahyar.etedal@icloud.com>
2018-11-10 12:05:39 -0500

    Solution to the first question for 2D arrays is added.

---
+++ Questions-Solutions/Two-Dimensional-Arrays/Q01MineSweeper.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+const int num_rows = 2;
+const int num_cols = 2;
+
+void minesweeper(int bombs[num_rows][num_cols]){
+  int field[num_rows][num_cols] ={0};
+  for(int i=0;i<num_rows;i++){
+    for(int j=0;j<num_cols;j++){
+      if(i = bombs[
+      fields[i][j] = -1;
+    }
+  }
+}
+
+
+int main(){
+  int bombs[num_rows][num_cols] = {{0,0},{1,1}};
+  minesweeper(bombs);
+}
