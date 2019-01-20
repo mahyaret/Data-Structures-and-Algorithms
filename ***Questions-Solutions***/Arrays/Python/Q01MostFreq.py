5bbe157f8d1127d0ba3781adf07ef3e1f2b2667c
mahyar.etedal@icloud.com <mahyar.etedal@icloud.com>
2019-01-20 12:37:33 -0500

    q1 python

---
+++ ***Questions-Solutions***/Arrays/Python/Q01MostFreq.py
@@ -0,0 +1,31 @@
+
+# Implement your function below.
+def most_frequent(given_list):
+    max_item = None
+    maxCount = -1
+    count = {}
+
+    for element in given_list:
+        if element not in count:
+            count[element] = 1
+        else:
+            count[element] += 1
+    for element in count:
+        if count[element]>maxCount:
+            maxCount = count[element]
+            max_item = element
+
+    return max_item
+
+# NOTE: The following input values will be used for testing your solution.
+# most_frequent(list1) should return 1
+list1 = [1, 3, 1, 3, 2, 1]
+print(most_frequent(list1))
+# most_frequent(list2) should return 3
+list2 = [3, 3, 1, 3, 2, 1]
+# most_frequent(list3) should return None
+list3 = []
+# most_frequent(list4) should return 0
+list4 = [0]
+# most_frequent(list5) should return -1
+list5 = [0, -1, 10, 10, -1, 10, -1, -1, -1, 1]
