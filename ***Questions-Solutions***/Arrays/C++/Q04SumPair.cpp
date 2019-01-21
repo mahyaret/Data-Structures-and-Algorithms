9d08df2285f1ff02914985805a7949eb16f5793b
mahyar.etedal@icloud.com <mahyar.etedal@icloud.com>
2019-01-21 18:32:03 -0500

    Q4 Solution added

--- ***Questions-Solutions***/Arrays/C++/Q04SumPair.cpp
+++ ***Questions-Solutions***/Arrays/C++/Q04SumPair.cpp
@@ -1,20 +1,36 @@
 #include <iostream>
-#include <list>
+#include <vector>
+#include <cmath>
+#include <algorithm>
 
-
-void findPair(std::list<int> array, int sum)
+void findPair(std::vector<int> array, int sum)
 {
-  array.sort();
-  for (auto element:array)
+  std::sort(array.begin(), array.end());
+  int startIndex = 0;
+  int endIndex = array.size()-1;
+
+  while(startIndex < endIndex)
     {
-      std::cout<<element<<" ";
+      if(array[startIndex] + array[endIndex] == sum)
+	{
+	  std::cout<<array[startIndex]<<","<<array[endIndex]<<std::endl;
+	  startIndex ++;
+	  endIndex --;
+	}
+      if((sum - array[startIndex]) > array[endIndex])
+	{
+	  startIndex ++;
+	}
+      if((sum - array[startIndex]) < array[endIndex])
+	{
+	  endIndex --;
+	}
     }
-  std::cout<<"Pair Not Found!"<<std::endl;
 }
 
 int main()
 {
-  std::list<int> array = { 8, 7, 2, 5, 3, 1};
+  std::vector<int> array = { 8, 7, 2, 5, 3, 1};
   int sum = 10;
 
   findPair(array, sum);
